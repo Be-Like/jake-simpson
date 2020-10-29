71e2d384b789cac1f9bc3c4b9174085f3bb6e089
Jacob Simpson <simpson.jacob.barry@gmail.com>
2020-10-29 10:49:06 -0400

    Deployment setup

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+yarn run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:Be-Like/jake-simpson.git master:gh-pages
+
+cd -
